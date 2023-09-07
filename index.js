9f200c853e4b14bbbb02b94f823dd29d6dc7a085
sasibushang <52733189+sasibushang@users.noreply.github.com>
2023-09-07 23:11:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello Sasi!'); //write a response to the client
+  res.write('Hello Sasi1!'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
