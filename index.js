91f47204b4c280273d3338c8d765ae9994559dae
sasibushang <52733189+sasibushang@users.noreply.github.com>
2023-09-08 20:26:19 +0530

    email test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello Sasi BG this is working??');
+  res.write('Hello Sasi BG check if emails wroking');
   res.end(); //Sasi ff
 }).listen(3000);
