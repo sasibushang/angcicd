5b61c850e58ec4aefcd7c0204fcd2e25a9045b96
sasibushang <52733189+sasibushang@users.noreply.github.com>
2023-09-08 17:14:30 +0530

    Update index.js
    Commit 1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello This is Working!!');
+  res.write('Hello This is working!!');
   res.end(); //Sasi ff
 }).listen(3000);
